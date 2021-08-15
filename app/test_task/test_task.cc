7991eb42ee09686af69fb5c5db0d33498cb0b5d1
Thiago Sant Helena <shthiago@users.noreply.github.com>
2021-08-15 14:49:31 -0300

    Implement test for task

---
+++ app/test_task/test_task.cc
@@ -0,0 +1,85 @@
+#include <time.h>
+#include <process.h>
+
+using namespace EPOS;
+OStream cout;
+
+int func_thread_a() {
+    cout << "This is thread A! Suspendin..." << endl;
+    Thread::self()->suspend();
+
+    cout << "This is thread A again! Returning..." << endl;
+    return 0;
+}
+
+int func_thread_b() {
+    cout << "This is thread B! Suspendin..." << endl;
+    Thread::self()->suspend();
+
+    cout << "This is thread B again! Returning..." << endl;
+    return 1;
+}
+
+int func_thread_c() {
+    cout << "This is thread C! Suspendin..." << endl;
+    Thread::self()->suspend();
+
+    cout << "This is thread C again! Returning..." << endl;
+    return 2;
+}
+
+
+int main() {
+    cout << "Testing Task creation" << endl;
+
+
+    Task * current_task = Task::self();
+
+    Segment * current_task_code_segment = current_task->code_segment();
+    CPU::Log_Addr current_task_code_init = current_task->code();
+    cout << "The code segment of the current task is at " << static_cast<void *>(current_task_code_init)
+         << " and it ends at " << static_cast<void *>(current_task_code_init + current_task_code_segment->size())
+         << endl;
+
+
+    Segment * current_task_data_segment = current_task->data_segment();
+    CPU::Log_Addr current_task_data_init = current_task->data();
+    cout << "The code segment of the current task is at " << static_cast<void *>(current_task_data_init)
+         << " and it ends at " << static_cast<void *>(current_task_data_init + current_task_data_segment->size())
+         << endl;
+
+    Address_Space * current_task_address_space = current_task->address_space();
+    cout << "The current task address space (page directory) is at " << current_task_address_space->pd() << endl;
+
+    cout << endl << "-----" << endl << "Testing creation of 3 threads from task" << endl;
+
+    Thread * thread_a = new Thread(&func_thread_a);
+    Thread * thread_b = new Thread(&func_thread_b);
+    Thread * thread_c = new Thread(&func_thread_c);
+
+    cout << "Suspending main task..." << endl;
+    Thread * current_thread = Thread::self();
+    current_thread->suspend();
+
+    // TODO Test fork
+
+    cout << "This is main thread. Other threads ran and suspended themselves. Will wait them to finish after suspend" << endl;
+
+    Alarm::delay(10000000);
+
+    thread_a->resume();
+    Thread::yield();
+    thread_b->resume();
+    Thread::yield();
+    thread_c->resume();
+    
+    int thread_a_return = thread_a->join();
+    int thread_b_return = thread_b->join();
+    int thread_b_return = thread_c->join();
+
+    cout << "Thread a return " << thread_a_return << endl;
+    cout << "Thread b return " << thread_b_return << endl;
+    cout << "Thread c return " << thread_c_return << endl;
+
+    cout << "Test finished successfully!" << endl;
+}
